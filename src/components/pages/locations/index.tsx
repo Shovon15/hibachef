14e5b9077fc081b74875b1177f77f0338a7bb17e
shovon15 <shovonrnbd@gmail.com>
2026-04-30 09:55:42 +0600

    update

--- src/components/pages/locations/index.tsx
+++ src/components/pages/locations/index.tsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import { HighlightHeading } from "@/components/common/typography/HighlightHeading";
+import ContentContainer from "@/components/layout/container/contentContainer";
+import PageBanner from "@/components/layout/container/PageBanner";
+import React from "react";
+import LocationBannerBg from "@/assets/images/location-banner.png";
 
-type Props = {data?: any}
+type Props = { data?: any };
 
 const LocationPage = (props: Props) => {
   return (
-    <div>locations Page</div>
-  )
-}
+    <div>
+      <PageBanner
+        bgImage={LocationBannerBg}
+        title=""
+        title2="Discover our Locations"
+        description="Hibachi Chef at Home brings the essence of Japanese cuisine right to your doorstep. Discover our menu, crafted with the finest ingredients to deliver authentic hibachi flavors and aromas. From sizzling meats and fresh seafood to vibrant hibachi vegetables and fried rice, our offerings are designed to impress. Explore our pricing options to find the perfect package for your next event."
+      />
+      <ContentContainer>
+        <HighlightHeading
+          text={`Find us in your nearby  location!`}
+          highlight={["nearby ", "location!"]}
+          highlightClassName="text-primary "
+          className="text-center"
+        />
+      </ContentContainer>
+    </div>
+  );
+};
 
-export default LocationPage
+export default LocationPage;
