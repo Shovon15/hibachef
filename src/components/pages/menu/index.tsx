24123a451d342212e46204720d631b021a166a5f
Reduan413 <ridwanali3322@gmail.com>
2026-05-10 16:09:05 +0600

    update image

--- src/components/pages/menu/index.tsx
+++ src/components/pages/menu/index.tsx
@@ -4,6 +4,7 @@ import PackageSection from "./components/PackageSection";
 import PageBanner from "@/components/layout/container/PageBanner";
 import AdditionalInfoSection from "./components/AdditionalInfoSection";
 import { DEFAULT_IMAGE } from "@/components/common/image";
+import BlogBannerBg from "@/assets/images/blog-bg.png";
 import PerfectCelebration from "./components/PerfectCelebration";
 
 type Props = { data?: any };
@@ -12,7 +13,7 @@ const MenuPage = (props: Props) => {
   return (
     <div>
       <PageBanner
-        bgImage={DEFAULT_IMAGE}
+        bgImage={BlogBannerBg}
         title="Menu & Pricing"
         description="Hibachi Chef at Home brings the essence of Japanese cuisine right to your doorstep. Discover our menu, crafted with the finest ingredients to deliver authentic hibachi flavors and aromas. From sizzling meats and fresh seafood to vibrant hibachi vegetables and fried rice, our offerings are designed to impress. Explore our pricing options to find the perfect package for your next event."
       />
