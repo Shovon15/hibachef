68c6fe23f163385bb02e814c6742521722e2efe6
Reduan413 <ridwanali3322@gmail.com>
2026-05-10 15:40:32 +0600

    update

--- src/components/layout/header/HeaderComponent.tsx
+++ src/components/layout/header/HeaderComponent.tsx
@@ -25,7 +25,7 @@ const HeaderComponent = () => {
       className={`fixed top-7.5 w-full z-40 flex items-center transition-all duration-300 ease-in-out h-[60px] lg:h-[100px] lg:px-[0vw]
          2xl:px-[10.67vw]`}
     >
-      <header className="w-[92vw] mx-auto flex items-center justify-between h-full px-2 bg-[#ffffff]  rounded-full z-[9999]">
+      <header className="w-[92vw] mx-auto flex items-center justify-between h-full pl-2 pr-4 py-2 bg-[#ffffff]  rounded-full z-[9999]">
         <div className="flex items-center gap-3">
           <div className="flex items-center gap-4 md:pl-5 lg:hidden">
             <button
