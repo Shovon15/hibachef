fbf6857d17e0b91a69774d2fd861ac2324e734f7
Reduan413 <ridwanali3322@gmail.com>
2026-05-10 16:21:55 +0600

    update menu page

--- src/components/pages/menu/index.tsx
+++ src/components/pages/menu/index.tsx
@@ -14,8 +14,11 @@ const MenuPage = (props: Props) => {
     <div>
       <PageBanner
         bgImage={BlogBannerBg}
-        title="Menu & Pricing"
+        title="Menu "
+        title2="& Pricing"
         description="Hibachi Chef at Home brings the essence of Japanese cuisine right to your doorstep. Discover our menu, crafted with the finest ingredients to deliver authentic hibachi flavors and aromas. From sizzling meats and fresh seafood to vibrant hibachi vegetables and fried rice, our offerings are designed to impress. Explore our pricing options to find the perfect package for your next event."
+        mainTextColor="text-[#EE1F26]"
+        spanTextColor="text-white"
       />
       <PackageSection />
       <PerfectCelebration/>
